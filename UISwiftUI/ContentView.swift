a53c02da7c4bf06d3e8d516bac17705c06ea0a2c
Sheikh Bayazid <sheikhbayazid@gmail.com>
2021-04-21 14:20:20 +0600

    Initial Commit

---
+++ UISwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UISwiftUI
+//
+//  Created by Sheikh Bayazid on 4/21/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UISwiftUI/UISwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UISwiftUIApp.swift
+//  UISwiftUI
+//
+//  Created by Sheikh Bayazid on 4/21/21.
+//
+
+import SwiftUI
+
+@main
+struct UISwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
